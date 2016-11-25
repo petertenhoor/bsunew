8b7846155a8732cfbbec991d8efd5284ac041ba4
petertenhoor <P_T_H_92@hotmail.com>
2016-11-25 15:58:20 +0100

    fix build

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 server 'maseru.happy-online.nl', user: 'bsunew', roles: %w{web}
 
 set :application, 'BSU'
-set :repo_url, 'git@github.com:HappyOnline/bsu.git'
+set :repo_url, 'git@github.com:HappyOnline/bsunew.git'
 set :keep_releases, 2
 
 # Linked directories (WordPress):
