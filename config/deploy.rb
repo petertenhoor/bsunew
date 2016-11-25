c97815cd7ba947e60710ed408d150f23c9c5ae1b
petertenhoor <P_T_H_92@hotmail.com>
2016-11-25 16:10:21 +0100

    fix build staging

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 server 'maseru.happy-online.nl', user: 'bsunew', roles: %w{web}
 
-set :application, 'BSU new'
+set :application, 'BSU_new'
 set :repo_url, 'git@github.com:HappyOnline/bsunew.git'
 set :keep_releases, 2
 
